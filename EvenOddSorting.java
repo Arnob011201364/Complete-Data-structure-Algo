003ae68001ae6c2f75e6b0c5d329b73efb993ac5
Arnob <abarnab21@gmail.com>
2023-06-02 12:05:52 +0600

    new added

---
+++ EvenOddSorting.java
@@ -0,0 +1,111 @@
+import java.util.Arrays;
+public class EvenOddSorting {
+          static void swap(int arr[],int left , int right ){
+                    int temp =   arr[left];
+                     arr[left] =  arr[right];
+                     arr[right] = temp;
+           }
+          public static void sorting(int[] arr){
+                    int left = 0;
+                    int right = arr.length;
+                    while(left<right)//left<=right both are fine
+                    {
+                              if(arr[left]%2 == 1 && arr[right]%2==0){
+                                        swap(arr,left,right );
+                                        left++;
+                                        right--;
+                              }
+                              if(arr[left]%2==0)
+                              left++;
+                              if(arr[right]%2==1)
+                              right--;
+                    }
+          }
+          public static void printArray(int arr[]) {
+                    for (int num : arr) {
+                        System.out.print(num + " ");
+                    }
+                }
+            
+                public static void main(String[] args) {
+                    int arr[] = {2, 5, 3, 6, 9, 8, 6, 2, 1};
+                    sorting(arr);
+                    System.out.println("Sorted array:");
+                    Arrays.sort(arr);
+                    printArray(arr);         
+}
+}
+
+public class EvenOddSorting {
+          static void swap(int arr[],int left , int right ){
+                    int temp =   arr[left];
+                     arr[left] =  arr[right];
+                     arr[right] = temp;
+           }
+          public static void sorting(int[] arr){
+                    int left = 0;
+                    int right = arr.length;
+                    while(left<right)//left<=right both are fine
+                    {
+                              if(arr[left]%2 == 1 && arr[right]%2==0){
+                                        swap(arr,left,right );
+                                        left++;
+                                        right--;
+                              }
+                              if(arr[left]%2==0)
+                              left++;
+                              if(arr[right]%2==1)
+                              right--;
+                    }
+          }
+          public static void printArray(int arr[]) {
+                    for (int num : arr) {
+                        System.out.print(num + " ");
+                    }
+                }
+            
+                public static void main(String[] args) {
+                    int arr[] = {2, 5, 3, 6, 9, 8, 6, 2, 1};
+                    sorting(arr);
+                    System.out.println("Sorted array:");
+                    Arrays.sort(arr);
+                    printArray(arr);         
+}
+}
+
+public class EvenOddSorting {
+                    static void swap(int arr[],int left , int right ){
+                              int temp =   arr[left];
+                               arr[left] =  arr[right];
+                               arr[right] = temp;
+                     }
+                    public static void sorting(int[] arr){
+                              int left = 0;
+                              int right = arr.length;
+                              while(left<right)//left<=right both are fine
+                              {
+                                        if(arr[left]%2 == 1 && arr[right]%2==0){
+                                                  swap(arr,left,right );
+                                                  left++;
+                                                  right--;
+                                        }
+                                        if(arr[left]%2==0)
+                                        left++;
+                                        if(arr[right]%2==1)
+                                        right--;
+                              }
+                    }
+                    public static void printArray(int arr[]) {
+                              for (int num : arr) {
+                                  System.out.print(num + " ");
+                              }
+                          }
+                      
+                          public static void main(String[] args) {
+                              int arr[] = {2, 5, 3, 6, 9, 8, 6, 2, 1};
+                              sorting(arr);
+                              System.out.println("Sorted array:");
+                              Arrays.sort(arr);
+                              printArray(arr);         
+          }
+}
---
+++ ZeroOneSorting.java
@@ -0,0 +1,25 @@
+//brute force approach
+class ZeroOneSorting {
+          static int count = 0;
+          public static void ZeroOne(int arr[]){
+          for(int i = 0 ; i<arr.length ; i++){
+              if(arr[i] == 0)
+              count++;
+          }    
+}        static void sorting(int arr[]){
+          for(int i = 0 ;i <arr.length ; i++){
+          if(i<count){
+          System.out.print(0+" ");
+          }
+          else{
+          System.out.print(1+" ");
+          }
+}
+}
+          public static void main(String[] args) {
+          int arr[] = {0,0,1,1,1,1,0,0,0} ; 
+          ZeroOne(arr);
+          sorting(arr);
+          }
+
+}
